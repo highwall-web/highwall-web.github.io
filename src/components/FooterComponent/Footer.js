9ed30bf252997954554b33e49f7b26063091c057
highwall <mauropio.altamura@outlook.it>
2023-11-09 12:08:41 +0100

    DEV - Added icon8 rights in footer

--- src/components/FooterComponent/Footer.js
+++ src/components/FooterComponent/Footer.js
@@ -9,6 +9,7 @@ const Footer = () => {
                     <a href="https://www.linkedin.com/in/mauro-pio-altamura-5b5b6027a" target="_blank" rel="noreferrer">LinkedIn</a>
                     <a href="../../assets/CV-Mauro-Pio-Altamura.pdf" target="_blank" rel="noreferrer">CV</a>
                     <p className="rights">©{currentYear} All rights reserved.</p>
+                    <p className={"icons"}>All icons used are from <a href={"https://icons8.com/"}>icons8.com</a></p>
                 </nav>
         </footer>
     )
