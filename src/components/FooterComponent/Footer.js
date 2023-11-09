84cafa5d1bd6e6638590464136e2f453875c1128
highwall <mauropio.altamura@outlook.it>
2023-11-09 12:17:01 +0100

    DEV - Adjusted size and margin footer

--- src/components/FooterComponent/Footer.js
+++ src/components/FooterComponent/Footer.js
@@ -6,8 +6,10 @@ const Footer = () => {
     return (
         <footer>
                 <nav className="footer-content">
-                    <a href="https://www.linkedin.com/in/mauro-pio-altamura-5b5b6027a" target="_blank" rel="noreferrer">LinkedIn</a>
-                    <a href="../../assets/CV-Mauro-Pio-Altamura.pdf" target="_blank" rel="noreferrer">CV</a>
+                    <a className={"link"} href="https://www.linkedin.com/in/mauro-pio-altamura-5b5b6027a"
+                       target="_blank" rel="noreferrer">LinkedIn</a>
+                    <a className={"link"} href="../../assets/CV-Mauro-Pio-Altamura.pdf" target="_blank"
+                       rel="noreferrer">CV</a>
                     <p className="rights">©{currentYear} All rights reserved.</p>
                     <p className={"icons"}>All icons used are from <a href={"https://icons8.com/"}>icons8.com</a></p>
                 </nav>
